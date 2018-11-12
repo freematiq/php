6a16926829c8911539c04e41b6e71953232a7b55
Сергей <sergey.dorgavcev@stm-labs.ru>
2018-11-12 23:16:11 +0700

    03 syntax

---
+++ php/03. http/example.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Example</title>
+</head>
+<body>
+<?php
+
+echo 'GET<br>';
+var_dump($_GET);
+
+echo 'POST<br>';
+var_dump($_POST);
+
+echo 'SERVER<br>';
+var_dump($_SERVER);
+
+?>
+</body>
+</html>
